cbd00ead7fa40a9e0ff41238670378e114bb219c
erwan-beauchamps <49027437+erwan-beauchamps@users.noreply.github.com>
2023-03-23 15:20:20 +0100

    resultat

--- src/components/meteo-station/meteo-station.tsx
+++ src/components/meteo-station/meteo-station.tsx
@@ -1,4 +1,4 @@
-import { Component, State, h } from '@stencil/core';
+import { Component, State, Element, h } from '@stencil/core';
 
 @Component({
   tag: 'meteo-station',
@@ -8,9 +8,19 @@ import { Component, State, h } from '@stencil/core';
 export class MeteoStation {
   @State() meteoTemp: number;
 
-  onFetchMeteo(event: Event) {
-    event.preventDefault();
+  @State() cityValue: string;
 
+  cityInput: HTMLInputElement;
+  
+  @State() error: string;
+
+  onInpuCity(ev: Event) {
+    this.cityValue = (ev.target as HTMLInputElement).value;
+  }
+
+  onFetchMeteo(event: Event) {
+
+    event.preventDefault();
     fetch(
       'https://angular-initiation-default-rtdb.firebaseio.com/meteo.json'
     )
@@ -18,20 +28,40 @@ export class MeteoStation {
         return res.json();
       })
       .then(parsedRes => {
-        this.meteoTemp = +parsedRes['temp'];
+        if (!parsedRes[this.cityValue]) {
+          throw new Error('Aucune ville trouvée');
+        }
+        this.meteoTemp = +parsedRes[this.cityValue];
+        this.error = null;
       })
       .catch(err => {
         console.log(err);
+        this.error = err.message;
       });
   }
 
   render() {
+
+    let dataContent = <p>Cherchez une ville</p>;
+    if (this.error) {
+      dataContent = <p>{this.error}</p>;
+    }
+    if (this.meteoTemp && this.error === null) {
+      dataContent = <p>Température: {this.meteoTemp}°</p>;
+    }
+
     return [
       <form onSubmit={this.onFetchMeteo.bind(this)}>
+        <input 
+          id="city"
+          value={this.cityValue}
+          onInput={this.onInpuCity.bind(this)}
+          ref={el => this.cityInput = el}>
+        </input>
         <button type='submit'>Rafraichir</button>
       </form>,
       <div>
-        <p>Température: {this.meteoTemp}°C</p>
+        {dataContent}
       </div>
     ];
   }
